68a2acbd222c3de1abe4ca24053d9448c1e81b63
Calvin Hyundong Kim <calvin75@gmail.com>
2021-08-11 15:48:56 +0900

    Delete terraform_init.sh

--- terraform/terraform_init.sh
+++
@@ -1,2 +0,0 @@
-
-terraform init -backend-config="storage_account_name=hyuktf" -backend-config="container_name=tfstate" -backend-config="access_key=Hl/snWf2eIjft+J0uhQE+IVr6wfDjLcVvli4DhnSNgoaZld1PDNF247j8sxLto2zEJVWV7lcShsw00u1gKBuDg==" -backend-config="key=aks-hands-on.fstate"
