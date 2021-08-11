28f0e34f3ed30914b892662f3a26396a34091dd3
Calvin Hyundong Kim <calvin75@gmail.com>
2021-08-11 15:49:23 +0900

    Delete acr.tf

--- acr.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                     = "registry${var.cluster_name}"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  sku                      = "Standard"
-  admin_enabled            = false
-}
